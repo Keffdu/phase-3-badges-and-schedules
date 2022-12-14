c37ba84108bbee1a6fc004e423a169728d53352d
Kevin Dutzy <kddutz15@gmail.com>
2022-12-13 20:37:41 -0500

    commit tree 7d80209e1505b86e937e101baa65ecb2327ada56

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+require 'pry'
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index(1) do |name, index|
+        "Hello, #{name}! You\'ll be assigned to room #{index}!"
+    end
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).map {|badge| puts badge}
+    assign_rooms(attendees).map {|rooms| puts rooms}
+end
